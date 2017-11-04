4f63f594e89de22c931437d416fca50c980d6fc9
melvin.sorum <melvin.sorum@itggot.se>
2017-11-04 14:01:25 +0100

    Header och nav

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,31 @@
-x = [];
-y = [];
+function dontRun() {
 
-var times = rand();
-for(var i=0; i < times; i++){
-    x.push(i);
-    y.push(50 + rand());
-};
+    console.log("i am alvie")
 
-if (rand() > 10) {
-    var color = ["#64ffda", "#9effff"];
-} else {
-    var color = ["#40ff80", "#83ffb1"];
-};
+    x = [];
+    y = [];
+    
+    var times = Math.floor((Math.random() * 20) + 1);
+    for(var i=0; i < times; i++){
+        x.push(i);
+        y.push(50 + rand());
+    };
+    
+    if (Math.floor((Math.random() * 20) + 1) > 10) {
+        var color = ["#64ffda", "#9effff"];
+    } else {
+        var color = ["#40ff80", "#83ffb1"];
+    };
+    
+    drawGraph(x, y, color)
 
-drawGraph(x, y, color)
+}
 
 function rand() {
     return Math.floor((Math.random() * 20) + 1)
 };
 
-function drawGraph(x, y) {
+function drawGraph(x, y, color) {
     const CHART = document.getElementById('lineChart');
     let lineChart = new Chart(CHART, {
         type: 'line',
@@ -35,4 +41,33 @@ function drawGraph(x, y) {
             ],
         }
     });
-}
+};
+
+// Toggla skuggan
+function toggle_shadow() {
+    element = document.querySelector(".nav_main");
+    element.classList.toggle("nav_shadow_is_visible");
+};
+
+// Toggla osynliga leave
+function toggle_leave() {
+    element = document.querySelector(".nav_leave");
+    element.classList.toggle("nav_is_visible");
+};
+
+// Animera Hamburgar menyn
+$(document).ready(function() {
+    $("#burger").click(function() {
+        $("nav").animate({
+            left: '0px'
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(".nav_leave").click(function() {
+        $("nav").animate({
+            left: '-304px'
+        });
+    });
+});
