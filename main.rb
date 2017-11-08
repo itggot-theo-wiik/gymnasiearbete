d8ee97b1544776a9c2aa6a8384cb9ec3294f6596
itggot-theo-wiik <theo.wiik@itggot.se>
2017-11-08 14:52:41 +0100

    formulär

--- main.rb
+++ main.rb
@@ -7,6 +7,21 @@ class Main < Sinatra::Base
         slim :register
     end
 
+    post '/register' do
+        day1 = params['day1']
+        day2 = params['day2']
+        day3 = params['day3']
+        day4 = params['day4']
+        day5 = params['day5']
+        day6 = params['day6']
+        day7 = params['day7']
+        puts "------------------------------"
+        puts day1
+        puts day2
+        puts "------------------------------"
+        redirect :register
+    end
+
     get '/users' do
         @users = Users.all
         slim :list
