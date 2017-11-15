64b14e02ef254b59b9707e7cd1620a12f45df569
itggot-theo-wiik <theo.wiik@itggot.se>
2017-11-15 14:21:16 +0100

    stuff

--- main.rb
+++ main.rb
@@ -3,11 +3,18 @@ class Main < Sinatra::Base
         slim :home
     end
 
+    get '/login' do
+        slim :login
+    end
+
     get '/register' do
         slim :register
     end
 
     post '/register' do
+        username = params['username_account']
+        password = params['password']
+        mail = params['mail']
         day1 = params['day1']
         day2 = params['day2']
         day3 = params['day3']
@@ -16,10 +23,11 @@ class Main < Sinatra::Base
         day6 = params['day6']
         day7 = params['day7']
         puts "------------------------------"
-        puts day1
-        puts day2
+        p day1
+        p day2
+        p day
         puts "------------------------------"
-        redirect :register
+        redirect :'/register'
     end
 
     get '/users' do
