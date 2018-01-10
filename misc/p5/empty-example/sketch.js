e523dc283fde2f273a3352ef0f4b14a4d349397a
itggot-theo-wiik <theo.wiik@itggot.se>
2018-01-10 14:45:31 +0100

    better graph

--- misc/p5/empty-example/sketch.js
+++ misc/p5/empty-example/sketch.js
@@ -7,12 +7,14 @@ function setup() {
 	sorted_y = [-10, 45, 45, 46.83, 110.8, 120, 140];
 
 	// Height and width of box/canvas
-	width = 1000;
-	height = 500;
+	width = 640;
+	height = 360;
+
+	circle_diameter = 13;
 
 	// Frame
 	height_percentage = 0.7;
-	width_percentage = 0.85;
+	width_percentage = 0.9;
 
 	// Create the canvas
 	createCanvas(width, height);
@@ -25,6 +27,7 @@ function setup() {
 function draw() {
 	// Background and axis lines (and the text)
 	background(12, 247, 161);
+	background(0, 202, 81);
 	// line(0, (height * 0.1), 0, (height * 0.9));
 	// text(sorted_y[y.length - 1].toString(), 0, height * 0.1 - 5);
 	// text(sorted_y[0].toString(), 0, height * 0.9 - 5);
@@ -37,20 +40,37 @@ function draw() {
 	// First line
 	current_x = 0.5 * (width - (width * width_percentage));
 	current_y = y_pos(0);
-	print("Current_y:")
-	print(current_y);
 
+	// Lines
 	i = 0;
-	while (i <= x.length) {
+	while (i < x.length) {
 		i++;
 		// The line
 		line(current_x, current_y, (current_x + step), y_pos(i));
+		strokeWeight(3);
+
+		textSize(16);
 
 		// The texts
 		// KG
 		text(y[i - 1] + " KG", current_x - 15, current_y - 15);
+		fill(255, 255, 255);
 		// DATE
 		text(x[i - 1], current_x - 15, height - 15);
+
+		// New positions
+		current_x = (current_x + step);
+		current_y = y_pos(i);
+	};
+
+	current_x = 0.5 * (width - (width * width_percentage));
+	current_y = y_pos(0);
+
+	// Dots
+	i = 0;
+	while (i < x.length) {
+		i++;
+		ellipse(current_x, current_y, circle_diameter, circle_diameter);
 
 		// New positions
 		current_x = (current_x + step);
